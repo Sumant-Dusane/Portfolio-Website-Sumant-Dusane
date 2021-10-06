e843039721dc513a4ef05182e45886a853d47e98
Sumant Dusane <78201887+Sumant-Dusane@users.noreply.github.com>
2021-10-06 11:05:04 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,20 +1,4 @@
 <?php
-// get data from form  
-
-// $name = $_POST['name'];
-// $email= $_POST['email'];
-// $message= $_POST['description'];
-// $to = "sumant.dusane1966@gmail.com";
-// $subject = $_POST['subject'];
-// $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-// $headers = "From: noreply@yoursite.com" . "\r\n" .
-// "CC: somebodyelse@example.com";
-// if($email!=NULL){
-//     mail($to,$subject,$txt,$headers);
-// }
-// redirect
-// header("Location:thankyou.html");
-
 
 if (isset($_POST['submit'])){
     $mailto = "sumant.dusane1966@gmail.com";
